f88a165531fe8e058c93a628415415af73407ccd
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-01-04 00:23:12 +0530

    menu done

--- src/components/cards/menuCard.tsx
+++ src/components/cards/menuCard.tsx
@@ -1,10 +1,74 @@
-interface MenuCardProps {
+import {motion, useAnimation} from "framer-motion";
+import RoundButton from "../roundButton.tsx";
 
+interface MenuCardProps {
+    image:string,
+    name:string,
+    price:number,
 }
 
-function MenuCard({ }:MenuCardProps) {
+function MenuCard({image,name,price}:MenuCardProps) {
+    const imageVariant =  {
+      initial:{
+
+      },
+      animate:{
+          scale:0.95,
+          filter: 'blur(1px)',
+          transition:{
+            duration:0.25,
+              ease:'easeInOut'
+          }
+      }
+    }
+    const showVariant =  {
+      initial:{
+            opacity:0
+      },
+      animate:{
+          opacity:1
+      }
+    }
+    const controls = useAnimation();
+
+    const handleHover = async () => {
+        await controls.start("animate");
+    };
+
+    const handleHoverEnd = async () => {
+        await controls.start("initial");
+    };
     return (
-        <div></div>
+        <motion.div
+            onHoverStart={handleHover}
+            onHoverEnd={handleHoverEnd}
+            className={"h-[25rem] my-5 bg-tertiary relative overflow-hidden rounded-3xl"}>
+           <motion.div className={"h-3/4 "}
+               variants={imageVariant}
+               initial={"initial"}
+               whileHover={"animate"}>
+               <img
+
+                   src={image} className={"object-cover scale-105 h-full "} alt=""/>
+           </motion.div>
+            <motion.div
+                variants={showVariant}
+                initial={"initial"}
+                animate={controls}
+                className={"absolute text-black text-4xl font-kadwa font-medium text-center px-3 text-shadow-sm shadow-red-300 cursor-context-menu w-full top-[5rem]"}>
+                {name}
+            </motion.div>
+            <motion.div
+                variants={showVariant}
+                initial={"initial"}
+                animate={controls}
+                className={"absolute text-black text-3xl font-kadwa font-medium text-center px-3 text-shadow-sm shadow-red-300 cursor-context-menu  w-full top-[10rem]"}>
+                Rs {price}
+            </motion.div>
+            <div className={"h-24 bottom-0 absolute w-full flex items-center justify-center rounded-b-2xl  bg-primary"}>
+                <RoundButton color={"quinary"} fontColor={"white"} text={"order now"}/>
+            </div>
+        </motion.div>
     );
 }
 
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,6 +1,10 @@
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
+import image1 from "../assets/delicious-cheeseburger.jpg";
+import image2 from "../assets/white-burer-2.jpg";
+import image3 from "../assets/white-burger-3.jpg";
 import {secondary} from "../state/colorReducer.ts";
+import MenuCard from "../components/cards/menuCard.tsx";
 
 
 function Menu() {
@@ -12,14 +16,26 @@ function Menu() {
     }, []);
 
     return (
-       <section className={"bg-secondary min-h-[100vh] w-full pt-28"}>
-            <div className={"mx-[10rem] border-black border-2 "}>
+        <section className={"bg-secondary min-h-[100vh] w-full pt-28"}>
+            <div className={"mx-[10rem]  "}>
                 <div className={"font-normal font-inter text-5xl text-center text-tertiary mb-28"}>menu</div>
-                <div className={"border-green-300 flex border-2 h-full"}>
+                <div className={" flex flex-wrap h-full"}>
+                    <div className={"w-full rounded-3xl overflow-hidden sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4"}>
+                        <MenuCard image={image1} name={"lorem"} price={100}/>
+                    </div>
+                    <div className={"w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4"}>
+                        <MenuCard image={image2} name={"cheese burger "} price={100}/>
+                    </div>
+                    <div className={"w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4"}>
+                        <MenuCard image={image3} name={"lorem"} price={100}/>
+                    </div>
+                    <div className={"w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4"}>
+                        <MenuCard image={image1} name={"lorem"} price={100}/>
+                    </div>
 
                 </div>
             </div>
-       </section>
+        </section>
     );
 }
 
