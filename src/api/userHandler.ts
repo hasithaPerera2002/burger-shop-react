b695e85d8f5e6b87963fd87b1bea9fe725e00139
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-01 23:27:52 +0530

    deployed

--- src/api/userHandler.ts
+++ src/api/userHandler.ts
@@ -44,11 +44,11 @@ const loginUser = async (email:string,password:string):Promise<AxiosResponse<any
 async function makeRequest(config:AxiosRequestConfig):Promise<AxiosResponse<any>> {
     try {
         const response = await axios.request(config);
-        console.log(JSON.stringify(response.data));
+        console.log(JSON.stringify(response.data),"response success");
         return  response;
     }
     catch (error) {
-        console.log(error);
+        console.log(error,"response error");
     }
     // @ts-ignore
 
